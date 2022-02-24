e4dd47ae4c1de51353027d7333359a729949ff20
Farhan <farhanskipper@gmail.com>
2022-02-24 21:24:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+// let & arrow function
+
+// function myName() {
+//     console.log("Farhan");
+// }
+// myName();
+
+// let sayName = (name) => {
+//     console.log(name);
+// }
+// sayName('farhan');
+
+// let myName = name => {
+//     return name;
+// }
+// console.log(myName('Farhan'));
+
+// let myName = name => name;
+// console.log(myName('Farhan'));
+
+// let double = num => num*2
+// console.log(double(10));
+
+// Export and Import (Modules)
+
+// import {hello} from './sell.js';
+// import {data} from './sell.js';
+// import * as bundle  from "./sell.js";
+
+
+// bundle.hello();
+// console.log(bundle.data);
+
+// import ps from "./customer.js"
+
+// console.log(ps);
+
+//class and object
+
+// class Customer{
+//     constructor(n){
+//         this.name = n;
+//     }
+//     age = 25;
+//     buy = () => {
+//         console.log(this.name)
+//     }
+// }
+
+// class guestCustomer extends Customer{
+//     hello(){
+//         console.log("hello");
+//     }
+// }
+
+// let customer1 = new guestCustomer("Farhan");
+
+// console.log(customer1);
+
+// customer1.buy();
+
---
+++ customer.js
@@ -0,0 +1,5 @@
+const person = {
+    name: "vishwajet"
+}
+
+export default person;
