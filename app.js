793f4152782fbac4f3fe43a103eeccf21658a7b5
Farhan <farhanskipper@gmail.com>
2022-02-25 13:00:30 +0500

    last commit

--- app.js
+++ app.js
@@ -37,20 +37,20 @@ console.log(ps); */
 
 //class and object
 
- /* class Customer{
-    constructor(n){
-        this.name = n;
-    }
-    age = 25;
-    buy = () => {
-        console.log(this.name)
-    }
+/* class Customer{
+   constructor(n){
+       this.name = n;
+   }
+   age = 25;
+   buy = () => {
+       console.log(this.name)
+   }
 }
 
 class guestCustomer extends Customer{
-    hello(){
-        console.log("hello");
-    }
+   hello(){
+       console.log("hello");
+   }
 }
 
 let customer1 = new guestCustomer("Farhan");
@@ -103,3 +103,51 @@ let {name , age} = person;
 
 console.log(name) */
 
+// Reference (Array , Object) and primitive type (string, number..)
+
+/* let num1 = 100;
+
+let num2 = num1;
+
+num2 = 50;
+
+console.log(num1);
+console.log(num2); */
+
+/* let person = {
+    name: "Farhan",
+}
+
+let person2 = {
+    ...person
+};
+
+person2.name = "Maqsood";
+
+console.log(person)
+console.log(person2) */
+
+//map 
+
+/* let array1 = [2, 5, 7, 10];
+
+let array2 = array1.map( x => x*2 );
+
+console.log(array2); */
+
+/* let array1 = [2, 5, 7, 10];
+
+let array2 = array1.filter( function(x) {
+
+    if(x%2 == 0){
+        return x;
+    }
+} );
+
+console.log(array2); */
+
+/* let array1 = [2, 5, 7, 10];
+
+let array2 = array1.filter( x => x%2 == 0);
+
+console.log(array2); */
